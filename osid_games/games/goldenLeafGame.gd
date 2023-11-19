67a8bd83f5073c42f3d0386d6b04f9fd96d93662
osid54 <osid.potato@gmail.com>
2023-11-19 12:44:55 -0500

    added leaf death

--- osid_games/games/goldenLeafGame.gd
+++ osid_games/games/goldenLeafGame.gd
@@ -3,9 +3,11 @@ extends Microgame
 @export var leaf : PackedScene
 var leaves := []
 var density := 80
+var xInterval : int
+var yInterval : int
 func _on_start_game():
-	var xInterval = int(get_viewport().get_window().size.x/float(density))
-	var yInterval = int((get_viewport().get_window().size.y-64)/float(density))
+	xInterval = int(get_viewport().get_window().size.x/float(density))
+	yInterval = int((get_viewport().get_window().size.y-64)/float(density))
 	for i in yInterval:
 		leaves.append([])
 		for j in xInterval:
@@ -17,9 +19,13 @@ func _on_start_game():
 	$Selector.position = Vector2(get_viewport().get_window().size.x/2.0, (get_viewport().get_window().size.y-64)/2.0)
 	$Selector.visible = true
 
-func _process(_delta):
-	if Input.is_action_just_pressed("action"):
-		if $Selector.checkLeaf():
-			won.emit()
-		else:
-			lost.emit()
+func _on_timeout():
+	var win = $Selector.checkLeaf()
+	for i in yInterval:
+		for j in xInterval:
+			leaves[i][j].kill()
+	await get_tree().create_timer(1).timeout
+	if win:
+		won.emit()
+	else:
+		lost.emit()
