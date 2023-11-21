a72a83b19872bec159146f1e6b35f678cfde72b6
osid54 <osid.potato@gmail.com>
2023-11-20 22:09:14 -0500

    added bg

--- osid_games/swordGame/main/swordDuelGame.gd
+++ osid_games/swordGame/main/swordDuelGame.gd
@@ -1,23 +1,51 @@
 extends Microgame
 
 var go := false
-var lose := false
+var lose := true
+var attack := false
 
 func _process(_delta):
-	if lose:
+	if attack:
 		return
 	if Input.is_action_just_pressed("action"):
 		hit()
 
 func _on_start_game():
-	await get_tree().create_timer(randf_range(.2*%MicrogameTimer.wait_time,.9*%MicrogameTimer.wait_time)).timeout
+	await get_tree().create_timer(randf_range(.2*$MicrogameTimer.wait_time,.8*$MicrogameTimer.wait_time)).timeout
+	if attack:
+		return
 	go = true
-	await get_tree().create_timer(.5).timeout
+	#eyes pop in, unsheath
+	$enemy.play("unsheath")
+	await get_tree().create_timer(1).timeout
+	go = false
+	if !attack and lose:
+		print("!attack and lose")
+		#stabbed animation
+		$enemy.position.x -= 500
+		$enemy.play("sheath")
+		await $enemy.animation_finished
+		lost.emit()
+	elif !attack and !lose:
+		#cross animation
+		print("!attack and !lose")
+		$enemy.position.x -= 500
+		$enemy.play("sheath")
+		await $enemy.animation_finished
+		won.emit()
 
 func hit():
-	if !go:
-		lose = true
-		lost.emit()
+	if !attack:
+		attack = true
+		if go:
+			lose = false
+		else:
+			#attack but stabbed
+			print("attack and lose")
+			lost.emit()
+	go = false
 
 func _on_timeout():
-	lost.emit()
+	pass
+#	if !lose:
+#		lost.emit()
